9ed437b6161e7be5cef0f229afe2c809da8cf998
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-02 19:57:18 +0100

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" returns the floor of the float. """
+
+
+def sum_list(input_list: list) -> float:
+    """ returns the floor of the float. """
+    sum_total = 0
+    for num in input_list:
+        sum_total += num
+    return sum_total
